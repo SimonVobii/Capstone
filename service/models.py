76f136a7598ea0dc79ef669e7f92199e9d958b05
Pretendi <paul.sfrt@gmail.com>
2018-11-25 21:23:09 +0000

    updated models according to Hassan's feedback

--- service/models.py
+++ service/models.py
@@ -3,25 +3,49 @@ from django.contrib.auth.models	import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from account.models import *
+from django.utils import timezone
 
 class PortfolioID(models.Model):
+#connects users to their saved portfolios. Also contains information on the portfolio's goals and time horizon
 	portfolioID = models.AutoField(primary_key=True)
 	userID = models.ForeignKey(User, on_delete=models.CASCADE)
+	goalValue = models.FloatField(default = 0.05)
+	timeHorizon = models.DateField(default=timezone.now)
 
 class stockID(models.Model):
+#contains information on particular stocks that is not appropriate to be stored elsewhere
 	tickerID = models.AutoField(primary_key=True)
 	companyName = models.TextField()
+	ipoDate = models.DateField(default=timezone.now)
 
 class PortfolioWeights(models.Model):
+#contains the current asset weights that define each portfolio
 	weightID = models.AutoField(primary_key=True)
 	portfolioID = models.ForeignKey(PortfolioID, on_delete=models.CASCADE)
 	tickerID = models.ForeignKey(stockID, on_delete=models.CASCADE)
+	volume = models.FloatField(default = 0)
+
+	class Meta:
+	#while django doesn't support true multi primary key, this gives us the desired functionality
+		unique_together = (('tickerID','portfolioID'))
 
 class stockHistory(models.Model):
+#largest data table, contains daily price for each asset. Note that this uses
+#a double primary key for easier indexing
 	eventID = models.AutoField(primary_key=True)
 	tickerID = models.ForeignKey(stockID, on_delete=models.CASCADE)
+	dateID = models.DateField(default=timezone.now)
+	assetPrice = models.FloatField(default = 0)
+	assetReturn = models.FloatField(default = 0)
+
+	class Meta:
+	#while django doesn't support true multi primary key, this gives us the desired functionality
+		unique_together = (('tickerID','dateID'))
 
 class portfolioHistory(models.Model):
+#contains a complete history of portfolio rebalancing (when the total volume of an asset changed,
+#but now when the value of an asset holding varied in respect to total portfolio value)
 	rebalanceID = models.AutoField(primary_key=True)
 	portfolioID = models.ForeignKey(PortfolioID, on_delete=models.CASCADE)
 	tickerID = models.ForeignKey(stockID, on_delete=models.CASCADE)
+	dateID = models.DateField(auto_now=True)
