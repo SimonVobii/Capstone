45002be6d0639c08bb08e61f7b444116b20d4fc5
Pretendi <paul.sfrt@gmail.com>
2018-11-26 00:40:57 +0000

    ready for stock data uploading

---
+++ scripts/stockHistory_load.py
@@ -0,0 +1,17 @@
+import csv
+import service.models as service_models
+
+def run(*args):
+	if 'load' in args:
+		with open("raw_data/stockret_3.csv", newline='') as csvfile:
+			reader = csv.reader(csvfile, delimiter=',')
+			next(reader)
+			counter = 0
+			for row in reader:
+				x = service_models.stockHistory(dateID = row[0], tickerID=service_models.stockID.objects.get(tickerID=str(row[1])), assetPrice=row[2], assetReturn=row[3])
+				x.save()
+				counter += 1
+				if (counter%10000 == 0):
+					print(counter)
+	if 'delete' in args:
+		service_models.stockHistory.objects.all().delete()
